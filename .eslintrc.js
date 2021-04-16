6ba1624c622a3ffacaa939a5e37e63ce629e18b3
xiekai01 <xiekai01@baidu.com>
2021-04-16 13:12:50 +0800

    chore: add lint & format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true
     },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint',
-    'react'
-  ],
-  settings: {
-    react: {
-      version: 'detect'
+    extends: [
+        'plugin:react/recommended',
+        'standard',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        quotes: ['error', 'single']
     }
-  },
-  rules: {
-    quotes: ['error', 'single']
-  }
 }
